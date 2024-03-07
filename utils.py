69f4b79aebdbefaa47728c4da58774a8007db6d7
Sem <semseveryanin@gmail.com>
2024-03-08 00:08:58 +0300

    added docstrings

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import psycopg2
 
 
-def create_database(database_name: str, params: dict):
+def create_database(database_name: str, params: dict) -> None:
+    """
+    Функция создания БД
+    :param database_name: название БД
+    :param params: параметры для подключения к БД
+    :return: None
+    """
     conn = psycopg2.connect(dbname='postgres', **params)
     conn.autocommit = True
     cur = conn.cursor()
@@ -17,8 +23,7 @@ def create_database(database_name: str, params: dict):
         cur.execute("""
             CREATE TABLE employers (
                 employer_id INT PRIMARY KEY,
-                title VARCHAR(255) NOT NULL,
-                open_vacancies INTEGER,
+                company_name VARCHAR(255) NOT NULL,
                 city VARCHAR(50),
                 site_url TEXT,
                 description TEXT    
@@ -30,10 +35,10 @@ def create_database(database_name: str, params: dict):
             CREATE TABLE vacancies (
                 vacancy_id INT PRIMARY KEY,
                 employer_id INT REFERENCES employers(employer_id),
-                title VARCHAR(255) NOT NULL,
+                vacancy_name VARCHAR(255) NOT NULL,
                 salary_from INT,
                 salary_to INT,
-                currency VARCHAR(3),
+                valuta VARCHAR(3),
                 experience VARCHAR(50),
                 city VARCHAR(50),
                 vacancy_url TEXT 
@@ -43,21 +48,28 @@ def create_database(database_name: str, params: dict):
     conn.close()
 
 
-def save_data_to_database(data: list, database_name: str, params: dict):
+def save_data_to_database(data: list, database_name: str, params: dict) -> None:
+    """
+    Функция сохранения данных в БД
+    :param data: данные для сохранения
+    :param database_name: название БД
+    :param params: параметры для подключения к БД
+    :return: None
+    """
     conn = psycopg2.connect(dbname=database_name, **params)
     with conn.cursor() as cur:
         for employer in data:
             cur.execute("""
-                INSERT INTO employers (employer_id, title, open_vacancies, city, site_url, description)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO employers (employer_id, company_name, city, site_url, description)
+                VALUES (%s, %s, %s, %s, %s)
                 """,
-                        (employer['id'], employer['name'], employer['open_vacancies'], employer['area'],
-                            employer['site_url'], employer['description'])
+                        (employer['id'], employer['name'], employer['area'], employer['site_url'],
+                         employer['description'])
                         )
             for vacancy in employer['vacancies']:
                 cur.execute("""
-                    INSERT INTO vacancies (vacancy_id, employer_id, title, salary_from, salary_to, currency, experience,
-                    city, vacancy_url)
+                    INSERT INTO vacancies (vacancy_id, employer_id, vacancy_name, salary_from, salary_to, valuta, 
+                    experience, city, vacancy_url)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                     """, (vacancy['id'], employer['id'], vacancy['name'], vacancy['salary_from'], vacancy['salary_to'],
                           vacancy['currency'], vacancy['experience'], vacancy['area'], vacancy['url_vacancy'])
